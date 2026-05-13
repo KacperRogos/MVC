53656fd132ae1b02d01db9fa6a8b3e39177ff7e4
KOMPUTER\rogos <rogoskacper@gmail.com>
2026-05-13 14:29:31 +0200

    Search engine

--- Project/tickedBookingSystem/src/app/event-list/event-list.ts
+++ Project/tickedBookingSystem/src/app/event-list/event-list.ts
@@ -1,26 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { EventService } from '../services/event.service';
 import { Event } from '../models/event.model';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-event-list',
-  imports: [RouterLink, CommonModule],
+  imports: [RouterLink, CommonModule, FormsModule],
   templateUrl: './event-list.html',
   styleUrl: './event-list.css',
 })
 export class EventList implements OnInit {
-  events: Event[]=[];
-  constructor(private eventService: EventService) {}
+  events: Event[] = [];
+  searchText: string = '';
+
+  constructor(private eventService: EventService, public authService: AuthService) {}
 
   ngOnInit(): void {
     this.events = this.eventService.getAll();
+  }
+
+  filteredEvents(): Event[] {
+    return this.events.filter(e =>
+      e.name.toLowerCase().includes(this.searchText.toLowerCase())
+    );
   }
 
   delete(id: number): void {
     this.eventService.delete(id);
     this.events = this.eventService.getAll();
   }
-
 }
